6e4b0ddac1a687f5d09711edf1e994f5506a8ffa
Michael Goldstein <mlgoldstein28@gmail.com>
2023-05-12 10:56:29 -0400

    Navbar UI

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,6 @@ import logo from '../../images/Chipotle_Mexican_Grill_logo.svg.png';
 import secondaryLogo from '../../images/Screen Shot 2023-05-11 at 10.49.46 AM.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
 import bag from '../../images/Screen Shot 2023-05-11 at 5.45.56 PM.png';
 
 const Navbar = () => {
@@ -21,20 +20,20 @@ const Navbar = () => {
                 <FontAwesomeIcon className='ms-3 fs-4' icon={faUser} />
                 <span className={` ${styles.signInText} m-3 fw-bold`}> SIGN IN</span>
           </div>
-          <div className={`navbar-nav m-auto fw-bold`}>
+          <div className={`navbar-nav ms-auto fw-bold`}>
             <a className={`nav-item nav-link ${styles.tabs}`} href="/">ORDER</a>
             <a className={`nav-item nav-link ${styles.tabs}`} href="/">CATERING</a>
             <a class={`nav-item nav-link ${styles.tabs}`} href="/">REWARDS</a>
             <a className={`nav-item nav-link ${styles.tabs}`} href="/">OUR VALUES</a>
             <a className={`nav-item nav-link ${styles.tabs} me-2`} href="/">NUTRITION</a>
-            <button className={`${styles.findButton} fs-6 me-4`}>
+          </div>
+            <div className="ms-1">
+              <button className={`${styles.findButton} fs-6 me-5`}>
                 <span className=""><img src={secondaryLogo} alt="logo" className={`${styles.secondLogo} m-auto`} /></span>
                 <span className="fs-4"> | </span><span className={styles.buttonText}>FIND A CHIPOTLE</span>
-            </button>
-            <div className="ms-3">
-              <img src={bag} alt='cart' className="" style={{width: "21px", height: "33px"}} />
+              </button>
+                <img src={bag} alt='cart' className="me-5" style={{width: "21px", height: "33px"}} />
             </div>
-          </div>
         </nav>
     </div>
     )

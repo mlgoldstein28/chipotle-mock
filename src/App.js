e7534d8396afa5e030837bf2d0cb5a628525bc42
Michael Goldstein <mlgoldstein28@gmail.com>
2023-05-15 09:30:56 -0400

    Video and Sub-Header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Navbar from './components/Navbar/Navbar';
+import LeadPage from './components/LeadPage/LeadPage';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <LeadPage />
     </div>
   );
 }
---
+++ src/components/LeadPage/LeadPage.js
@@ -0,0 +1,36 @@
+import videoPastor from '../../images/Chipotle_ChickenAlPastor.mp4';
+import React from 'react';
+import styles from './LeadPage.module.scss';
+import chipBadge from '../../images/chipBadge.png'
+
+const LeadPage = () => {
+    return(
+        <div className="">
+            <div className={`ratio ${styles.vidContainer}`}>
+                <video
+                    autoplay=""
+                    muted 
+                    loop 
+                    className={`w-100 ${styles.vid}`}
+                    src={videoPastor} 
+                    type="video/mp4"  
+                      >
+                </video>
+            </div>
+            <div className="text-center mt-3">
+              <div className={`text-center d-flex justify-content-center`}>
+              <img src={chipBadge} alt="" className={`${styles.badge}`}/>
+                <p className={` ${styles.subText}`}>JOIN CHIPOTLE REWARDS. UNLOCK FREE CHIPOTLE.</p>
+                    <button className={styles.account}>CREATE AN ACCOUNT</button>
+                  <div className={styles.subText}>
+                    <span className={styles.or}> OR </span>
+                    <a className={styles.signIn}> SIGN IN</a>
+                    </div>
+                </div>
+              </div>
+            <hr />
+        </div>
+    )
+}
+
+export default LeadPage;

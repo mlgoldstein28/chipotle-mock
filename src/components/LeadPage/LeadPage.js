9c59d747f4fb71d4dfd66860f75f08aabd7d2812
Michael Goldstein <mlgoldstein28@gmail.com>
2023-05-16 12:39:47 -0400

    Text Over Video

--- src/components/LeadPage/LeadPage.js
+++ src/components/LeadPage/LeadPage.js
@@ -16,13 +16,24 @@ const LeadPage = () => {
                     type="video/mp4"  
                       >
                 </video>
+                <div className={`text-center ${styles.vidText}`}>
+                  <div className={`${styles.new}`}>NEW</div>
+                  <div className={`${styles.vidHead}`}>
+                    <h1 className={`${styles.chicken}`}>CHICKEN</h1>
+                    <h1 className={`${styles.alPastor}`}>AL PASTOR</h1>
+                  </div>
+                  <h2 className="fw-bold">WHERE FIRE MEETS FLAVOR</h2>
+                  <p className="fs-3">The rich flavor of Chicken Al Pastor delivers <br/> the right amount of heat 
+                    balanced with a <br/> splash of pineapple, fresh lime, and cilantro.</p>
+                  <a href="#menu" className={`text-decoration-none`}><div className={`${styles.orderButton}`}>ORDER NOW</div></a>
+                </div>
             </div>
             <div className="text-center mt-1 mb-1">
               <div className={`text-center d-flex justify-content-center`}>
               <img src={chipBadge} alt="" className={`${styles.badge}`}/>
                 <p className={` ${styles.subText}`}>JOIN CHIPOTLE REWARDS. UNLOCK FREE CHIPOTLE.</p>
                     <button className={styles.account}>CREATE AN ACCOUNT</button>
-                  <div className={styles.subText}>
+                  <div className={styles.subText} id="menu">
                     <span className={styles.or}> OR </span>
                     <a className={styles.signIn}> SIGN IN</a>
                     </div>
